50c5243a46de6db03049864fe8b47a17633e7c53
Matheus De Oliveira Lourenço <matheuslourencoo@outlook.com>
2026-06-01 23:13:31 -0300

    fix: alinhando import com o nome do arquivo header minúsculo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Header } from "./components/Header";
+import { Header } from "./components/header";
 import { Footer } from "./components/Footer";
 import { LoginModal } from "./components/LoginModal";
 import { ToastNotification } from "./components/ToastNotification";
